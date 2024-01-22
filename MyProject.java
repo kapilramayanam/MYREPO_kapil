0a7f17c4e64836fc17e26ffaad21b4508879c3d9
kapilramayanam <142371296+kapilramayanam@users.noreply.github.com>
2024-01-22 08:45:25 -0600

    My Info
    My name and my email

---
+++ MyProject.java
@@ -0,0 +1,13 @@
+/**
+ * myProject
+ */
+public class MyProject {
+    public static void main(String[] args) {
+
+    System.out.println("Name: Kapil Ramayanam");
+    System.out.println("Email : Kramayanam@murraystate.edu");
+    
+}
+
+    
+}
